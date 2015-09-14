b0b275a1a78f2378f41f090a12b1138def7c2d9f
Keita Yamada <yshiro9@yahoo.co.jp>
2015-09-15 01:13:42 +0900

    最初のサンプル

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var mainCtrl = function($scope){
+    $scope.users = [
+        {"name":"taguchi", "score":52.22},
+        {"name":"tanaka", "score":38.22},
+        {"name":"yamada", "score":11.22},
+];
+}
